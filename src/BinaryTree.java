226c9d9580b6f1b91c2f3d89cb13daeb303e2659
Technical-Faraz <wonderfaraz@gmail.com>
2021-03-21 20:58:50 +0500

    BinaryTree Creation

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,9 +1,29 @@
 
-public class BinaryTree {
+class Node{
+	int number;
+	Node left;
+	Node right;
+}
 
+public class BinaryTree {
+	public static Node createTree(Node parent, int depth, int data) {
+		if(depth != 5) {
+			parent = new Node();
+			parent.number = data;
+			parent.left = createTree(parent.left,depth+1,data+data);
+			parent.right = createTree(parent.right,depth+1,data+data+1);
+			return parent;
+		}
+		else {
+			return null;
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Node n = new Node();
+		n = createTree(n, 0, 1);
+		int x = n.right.left.left.number;
+		System.out.println(x);
+		
 	}
-
 }
